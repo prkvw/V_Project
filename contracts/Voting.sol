188191004daae497506e5b27882efbbb85b43c93
prkvw <neeqash@gmail.com>
2023-08-31 08:26:40 +0000

    deploy script

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,33 +1,60 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.8.9 <0.9.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
 
 contract Voting {
-    function vote () public {
-    // check if voter is registered
-    mapping (address => bool) voters;
+    // Track Voter Registration
+    mapping(address => bool) voters;
+    // Track Voters Voted
+    mapping(address => bool) hasVoted;
+    // Track Candidates
+    mapping(address => Candidate) candidates;
 
-
-    //track voters voted
-    mapping (address => bool) hasVoted;
-
-    //Track candidates
-    mapping (address => bool) candidates;
-
-    //vote
-
-
-    require (voters[msg.sender],"voter is not registered");
-    // check if voter has not voted
-    require (hasVoted[msg.sender],"voter has already voted")
-    // check if candidate is valid
-    require ( candidates[msg.sender], "Invalid candidate")
-    // Increase candidate vote
-    // mark voter as voted
-    //Increase the vote tally
-
+    struct Candidate {
+        address candidateAddress;
+        string name;
+        uint voteCount;
     }
-    function addCandidates () public ()
+    // Track Votes Tally
+    uint totalVotes;
+    // Contract Owner
+    address owner;
 
-    function deleteCandidates () ()
-    
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // Register Voter
+    function registerVoter(address voterAddress) public {
+        // Check If Sender is Owner
+        require(owner == msg.sender, "Not Owner!");
+        // Register
+        voters[voterAddress] = true;
+    }
+
+    // Add Candidate
+    function addCandidate(address candidateAddress, string memory name) public {
+        // Check If Sender is Owner
+        require(owner == msg.sender, "Not Owner!");
+        // Add Candidate
+        candidates[candidateAddress] = Candidate(candidateAddress, name, 0);
+    }
+
+    // Vote
+    function vote(address candidateAddress) public {
+        // Check if voter is registered
+        require(voters[msg.sender], "Voter is not registered!");
+        // Check if voter has not voted
+        require(!hasVoted[msg.sender], "Voter has already voted!");
+        // Check if candidate is valid
+        require(
+            candidates[candidateAddress].candidateAddress == candidateAddress,
+            "Candidate not valid!"
+        );
+        // Increase candidate vote
+        candidates[candidateAddress].voteCount++;
+        // Mark voter as voted
+        voters[msg.sender] = true;
+        // Increase the vote tally
+        totalVotes++;
+    }
 }
