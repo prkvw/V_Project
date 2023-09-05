03c46ad70351e93b662cdbd9ec6c046352eb3cbe
sB <140225174+prkvw@users.noreply.github.com>
2023-09-05 14:32:45 +0000

    Update ERCToken.sol

--- contracts/ERCToken.sol
+++ contracts/ERCToken.sol
@@ -5,9 +5,12 @@ pragma solidity ^0.8.9
 contract ERCToken { 
 mapping address => uint256 ;_balance
 mapping address => mapping (address => uint) _allowance;
-event Transfer (address indexed _from, address indexed _to, uint256 _value)
+mapping address => mapping (address => bool) _approval;
 
- function name () public pure returns (string memory){ return "Web3 Bootcamp"; }
+event Transfer (address indexed _from, address indexed _to, uint256 _value)
+event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+ 
+function name () public pure returns (string memory){ return "Web3 Bootcamp"; }
  function symbol () public pure returns (string memory) { return "W3BC"; }
  function decimal () public view returns (uint8) { return 2 ;}
  function totalSupply () public view returns (uint256){ return 1000;}
@@ -28,7 +31,8 @@ event Transfer (address indexed _from, address indexed _to, uint256 _value)
   
   
    function approve(address _spender, uint256 _value) public returns (bool success){
-
+// emit 
+emit event Approval(address indexed _owner, address indexed _spender, uint256 _value)
 //set spender allowance
   _allowance[msg. sender][_spender] = _value;
 //return success
