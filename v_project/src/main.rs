9b03f05af8f594f5fe7327aaff843c856b18894d
prkvw <neeqash@gmail.com>
2024-08-08 15:25:33 +0000

    main.rs 1.0

--- v_project/src/main.rs
+++ v_project/src/main.rs
@@ -1,24 +1,50 @@
-mod network;
+fn main ( ) {
 
-#[tokio::main]
-async fn main () {
-    let local_addr = "localhost:8000";
-    let remote_addr = "localhost:8000";
-
-    //start the network listener in a separate task
-    let listener_task = tokio::spawn(async move {
-        network::start_network_listener(local_addr).await.unwrap(); 
-    });
-
-    //start the network connector in a separate task
-    let connector_task = tokio::spawn(async move
-    {
-        network::start_network_connector(remote_addr).await.unwrap();
-    });
-
-    // await both tasks
-    let _ = tokio::try_join!(listener_task, connector_task);
 }
+match match 
+{
+    Ok (_) => println!(" ")
+}   Err (Error) => {
+    println!("Error: {}", Error );
+}
+//}
+//}
+fn start_client (socket_address: String) -> IOResult<()> {
+
+//client loop
+loop { 
+    // Read user input.
+    let outgoing = {
+        let 
+        let
+    }
+}
+
+//Tx user input to writer 
+let _ = 
+
+//Rx response from reader.
+let incoming = {
+    let 
+    let
+
+
+} ; 
+
+let display_msg = String::from_utf8_lossy(&);
+let display_msg = display_msg.trim();
+let reset = SgrCode::Reset.to_string();
+let display_msg = format!("{}", display_msg, reset);
+println!("{}", display_msg);
+
+//print debug
+log::info!(); 
+
+}
+
+
+
+
 
 
 // new features : peer discovery, message passing between peers, err(e)
