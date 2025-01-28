a991bd9ca29bc33baf0db64c906d27b9d2bdcc88
prkvw <neeqash@gmail.com>
2025-01-28 12:32:33 +0100

    Add Home Controller

---
+++ frontend/HomeController.cs
@@ -0,0 +1,10 @@
+namespace myWebapp.controllers { 
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+        private string JoinTwoStrings(string a, string b) {
+           var NewString = string.Concat (one,two);
+            return NewString ;
+        }
+}
